b26a1b592873ea4632aaddc4b75f14eeaedccaf5
nulysses <adr.zivil@gmail.com>
2021-05-20 07:59:04 +0200

    1

--- event_hooks/handlers/headphone-jack-handler.sh
+++ event_hooks/handlers/headphone-jack-handler.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo $4
-pactl set-sink-port 0 analog-output-headphones
+#echo $4
+if [ "$3" = "unplug" ]; then
+  pactl set-sink-port 0 analog-output-headphones
+else
+  pactl set-sink-port 0 analog-output-speaker
+fi
 
-echo "$@" > /tmp/test
+#echo "$@" > /tmp/test
