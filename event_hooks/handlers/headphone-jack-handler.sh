d261b2d05915bd29a95fd2fe1c9e904188db166e
nulysses <adr.zivil@gmail.com>
2021-05-20 08:30:00 +0200

    1

--- event_hooks/handlers/headphone-jack-handler.sh
+++ event_hooks/handlers/headphone-jack-handler.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 #echo $4
-if [ "$3" = "unplug" ]; then
-  sleep 5 && pactl set-sink-port 0 analog-output-headphones
-else
-  sleep 5 && pactl set-sink-port 0 analog-output-speaker
-fi
+#user_name="$(logname)"
+#user_id="$(id -u "${user_name}")"
+user_id="$(ls "/run/user/" | head -1)"
+user_name="$(id -nu "${user_id}")"
 
-#echo "$@" > /tmp/test
+if [ "$3" = "plug" ]; then
+  sudo -u "${user_name}" XDG_RUNTIME_DIR="/run/user/${user_id}" pactl set-sink-port 0 analog-output-headphones
+else
+  sudo -u "${user_name}" XDG_RUNTIME_DIR="/run/user/${user_id}" pactl set-sink-port 0 analog-output-speaker
+fi
+#sudo -u "${user_name}" XDG_RUNTIME_DIR="/run/user/${user_id}" pactl set-sink-port 0 analog-output-speaker
+#  sleep 2 && su - nulysses -c "pactl set-sink-port 0 analog-output-speaker"
+
+#echo $(date) >> /tmp/test
+#echo "$@" >> /tmp/test
+#echo "${user_name}" >> /tmp/test
+#echo "${user_id}" >> /tmp/test
