2663a482ab608ce877cb102be9016d00cf1239bd
nulysses <adr.zivil@gmail.com>
2021-06-08 07:29:33 +0200

    1

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 . "${script_dir}/vars"
 
+mkdir -p "${install_dir}"
+
 rm -rf "${install_dir}/${module_name}"
 cp -rf "${script_dir}/${module_name}" "${install_dir}/${module_name}"
 chmod +x "${install_dir}/${module_name}/"* -R
